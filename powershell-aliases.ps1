9d074818230b3d751cd29649f5bd660e82dcb889
tjtolon <tatu.tolonen@gmail.com>
2022-07-13 13:52:04 +0300

    Add a good git log alias glgga

--- powershell-aliases.ps1
+++ powershell-aliases.ps1
@@ -1,2 +1,5 @@
 function Git-GetStatus { git status }
 Set-Alias -Name gst -Value Git-GetStatus
+
+function Git-GoodLog { git log --graph --decorate --all --oneline }
+Set-Alias -Name glgga -Value Git-GoodLog
