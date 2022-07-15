49fd11069b8a3cbd5d0f3a0909def69c71c46e1b
tjtolon <tatu.tolonen@gmail.com>
2022-07-15 15:10:16 +0300

    Update Profile to have WSL2 installed docker

--- Profile.ps1
+++ Profile.ps1
@@ -3,3 +3,8 @@ $workScriptPath = (Split-Path $Profile) + "\work-helpers.ps1"
 if (Test-Path $workScriptPath) {
   . $workScriptPath
 }
+
+$DOCKER_DISTRO = "Ubuntu"
+function docker {
+    wsl -d $DOCKER_DISTRO docker -H unix:///mnt/wsl/shared-docker/docker.sock @Args
+}
