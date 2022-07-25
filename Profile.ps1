50db51b75203656ac9fef710f7d41f5d231de5ed
Tatu Tolonen <tatu.tolonen@unikie.com>
2022-07-25 10:26:30 +0300

    stuff

--- Profile.ps1
+++ Profile.ps1
@@ -1,10 +1,8 @@
-. ((Split-path $profile)+ "\powershell-aliases.ps1")
+$ProfileDirectory = (Split-path $profile)
+
+. ($ProfileDirectory + "\powershell-aliases.ps1")
+
 $workScriptPath = (Split-Path $Profile) + "\work-helpers.ps1"
 if (Test-Path $workScriptPath) {
   . $workScriptPath
-}
-
-$DOCKER_DISTRO = "Ubuntu"
-function docker {
-    wsl -d $DOCKER_DISTRO docker -H unix:///mnt/wsl/shared-docker/docker.sock @Args
 }
--- powershell-aliases.ps1
+++ powershell-aliases.ps1
@@ -3,3 +3,12 @@ Set-Alias -Name gst -Value Git-GetStatus
 
 function Git-GoodLog { git log --graph --decorate --all --oneline }
 Set-Alias -Name glgga -Value Git-GoodLog
+
+function Git-Pull { git pull }
+Set-Alias -Name gpull -Value Git-Pull
+
+function Git-AddUpdated { git add -u }
+Set-Alias -Name gau -Value Git-AddUpdated
+
+function Git-Add { git add }
+Set-Alias -Name ga -Value Git-Add
